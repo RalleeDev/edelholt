c5d25c95f9b822092dd048954d17d0c5469a92e1
RalleeDev <109039505+RalleeDev@users.noreply.github.com>
2025-02-03 01:19:33 +0100

    removed p tag

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Page() {
     return (
     <div>
-        <h1>Hi, im <p className="">Rasmus Edelholt</p></h1>
+        <h1>Hi, im Rasmus Edelholt</h1>
         <h2>A Backend Engineer trying new things</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ut vestibulum nisi. Praesent interdum elementum metus nec ultrices. Phasellus vehicula nisl aliquam, vulputate ex eu, efficitur magna. Quisque mi sapien, efficitur vestibulum maximus in, varius ac nisi. Integer eget dolor a lectus vestibulum bibendum ut non mauris. Curabitur tortor libero, convallis at arcu viverra, luctus pharetra est. Etiam malesuada eget lorem et mollis. Nam sed mollis enim, sed lacinia massa. In lacinia, mauris ac efficitur bibendum, dui quam sagittis lorem, eu convallis purus nunc quis nulla. Vivamus ac purus molestie, gravida odio vel, blandit urna. Nulla mattis interdum sem nec dictum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris in laoreet augue, vulputate tristique lacus.</p>
     </div>
